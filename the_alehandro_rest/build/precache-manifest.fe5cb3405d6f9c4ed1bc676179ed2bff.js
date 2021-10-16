ffa33086ac0471155c2b66a618558f5fc237b17e
andros3c <alehandro4354@gmail.com>
2021-10-16 22:21:36 +0700

    pembaruan build

---
+++ the_alehandro_rest/build/precache-manifest.fe5cb3405d6f9c4ed1bc676179ed2bff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b51ef35acf3fcf775b6aa96b0b11d46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c4ce583a42009a49ea",
+    "url": "/static/css/main.bfdd5bfe.chunk.css"
+  },
+  {
+    "revision": "eb6d1bd9b30c43f91cb3",
+    "url": "/static/js/2.7dd782f1.chunk.js"
+  },
+  {
+    "revision": "ed6b1f36309f516b9d7e07fe91a74cd2",
+    "url": "/static/js/2.7dd782f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6362ffad4863dd0c161a",
+    "url": "/static/js/3.78f5718e.chunk.js"
+  },
+  {
+    "revision": "e4c4ce583a42009a49ea",
+    "url": "/static/js/main.52aa57ea.chunk.js"
+  },
+  {
+    "revision": "9f24e7aaf05bc9434c86",
+    "url": "/static/js/runtime-main.5a2a900f.js"
+  },
+  {
+    "revision": "22e8549280288b0506a2bf614193134d",
+    "url": "/static/media/ab.22e85492.png"
+  },
+  {
+    "revision": "eebd58a20563da9bb86e35febca1cda1",
+    "url": "/static/media/bb.eebd58a2.png"
+  },
+  {
+    "revision": "094d9d1d9cecf1b20730d6fc2296eaaf",
+    "url": "/static/media/motif.094d9d1d.png"
+  }
+]);
